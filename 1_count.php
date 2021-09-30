16cab4d3c4c18b194421d7ccd3bea9aace9dd6cc
Dana Sugu <dana.sugu@gmail.com>
2021-09-30 08:19:51 +0300

    Update 1_count.php

--- 1_count.php
+++ 1_count.php
@@ -1,6 +1,11 @@
 <?php 
-$array = ['deer','mouse','lion','tiger','elephant','wolf','monkey','human'];
-// echo count($array);
-for ($i=0; $i<count($array); $i++){
+$array = ['deer','mouse','lion','tiger','elephant','wolf','monkey','human',
+['green','orange','pink']];
+echo count($array, COUNT_NORMAL);
+echo  '//Normal count<br>';
+echo count($array, COUNT_RECURSIVE);
+echo  '//Recursive count<br>';
+echo '<br>';
+for($i=0;$i<count($array);$i++) {
   echo $array[$i] .'<br>';
 }
