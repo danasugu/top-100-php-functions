190abe9c87cb7fa1077fc9237bbefbd11220b07c
Dana Sugu <dana.sugu@gmail.com>
2021-09-30 08:48:45 +0300

    Update 2_is_array.php

--- 2_is_array.php
+++ 2_is_array.php
@@ -2,3 +2,14 @@
 // bool is_array (mixed $var)
 // finds whether the given var is an array
 
+$array = ['deer','mouse','lion','tiger','elephant','wolf','monkey','human', ['green','orange','pink']];
+
+// $array=75;
+
+if(is_array($array)){
+  for($i=0; $i<count($array); $i++){
+  echo $array[$i] .'<br />';
+}
+} else{
+  echo 'Not an array';
+}
