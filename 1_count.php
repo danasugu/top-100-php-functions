df054da8450a61bb2792886d523c1959f58ae728
Dana Sugu <dana.sugu@gmail.com>
2021-09-30 08:23:40 +0300

    update

--- 1_count.php
+++ 1_count.php
@@ -6,6 +6,7 @@ echo  '//Normal count<br>';
 echo count($array, COUNT_RECURSIVE);
 echo  '//Recursive count<br>';
 echo '<br>';
+// die;
 for($i=0;$i<count($array);$i++) {
   echo $array[$i] .'<br>';
 }
