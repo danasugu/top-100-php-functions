a357b516597945ea4a0bba09d4deac31090c5e51
Dana Sugu <dana.sugu@gmail.com>
2021-10-01 19:03:49 +0300

    Update 4_in_array.php

--- 4_in_array.php
+++ 4_in_array.php
@@ -2,4 +2,8 @@
 // in_array(mixed $needle, array $haystack, bool $strict = false): bool
 // Searches for needle in haystack using loose comparison unless strict is set.
 
-$array = ['deer','mouse','lion','tiger','elephant','wolf','monkey','human'];
+$array = [11.5, 'deer','mouse','lion','tiger','elephant','wolf','monkey','human'];
+
+if (in_array('11.5', $array, true)) {
+    echo 'the number is in the array';
+}
