68ba46b7c31454969ff5781750e5259174339472
Dana Sugu <dana.sugu@gmail.com>
2021-10-01 18:59:47 +0300

    Update 4_in_array.php

--- 4_in_array.php
+++ 4_in_array.php
@@ -1,2 +1,5 @@
+<?php 
 // in_array(mixed $needle, array $haystack, bool $strict = false): bool
 // Searches for needle in haystack using loose comparison unless strict is set.
+
+$array = ['deer','mouse','lion','tiger','elephant','wolf','monkey','human'];
