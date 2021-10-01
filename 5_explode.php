15e8dd830f7f8fbe8ade1f7ceafe45a99f0147f9
Dana Sugu <dana.sugu@gmail.com>
2021-10-01 19:08:02 +0300

    Update 5_explode.php

---
+++ 5_explode.php
@@ -0,0 +1,5 @@
+<?php
+
+//explode(string $separator, string $string, int $limit = PHP_INT_MAX): array
+//Returns an array of strings, each of which is a substring of string formed by splitting it on boundaries formed by the string separator.
+
