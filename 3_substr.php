e39f800ce500ee5bebb2e8ef7c90229d67c7748b
Dana Sugu <dana.sugu@gmail.com>
2021-10-01 18:19:34 +0300

    Update 3_substr.php

--- 3_substr.php
+++ 3_substr.php
@@ -1,1 +1,9 @@
+<?php 
 $array = ['deer','mouse','lion','tiger','elephant','wolf','monkey','human'];
+
+// substr(string $string, int $offset, ?int $length = null): string
+//substr — Return part of a string
+
+$text = "this is a string";
+$extract = substr($text, 0);
+echo $extract;
