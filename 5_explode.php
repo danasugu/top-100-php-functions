f696a93bdff82896f1ae1cc64d89aa3a99159f24
Dana Sugu <dana.sugu@gmail.com>
2021-10-02 18:48:50 +0300

    Update 5_explode.php

--- 5_explode.php
+++ 5_explode.php
@@ -3,3 +3,7 @@
 //explode(string $separator, string $string, int $limit = PHP_INT_MAX): array
 //Returns an array of strings, each of which is a substring of string formed by splitting it on boundaries formed by the string separator.
 
+$array = [11.5, 'deer','mouse','lion','tiger','elephant','wolf','monkey','human'];
+
+$colors = "orange, green, cyan, magenta, pink, violet";
+
