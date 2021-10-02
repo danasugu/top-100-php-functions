c72a5bc413afc1bda267606ca56dc98985ae6346
Dana Sugu <dana.sugu@gmail.com>
2021-10-02 20:01:18 +0300

    Update 5_explode.php

--- 5_explode.php
+++ 5_explode.php
@@ -16,3 +16,18 @@ print_r($colors_array);
 $names_array = explode("|", $names);
 echo "<pre>";
 print_r ($names_array);
+
+// Example 1
+$pizza  = "piece1 piece2 piece3 piece4 piece5 piece6";
+$pieces = explode(" ", $pizza);
+echo $pieces[0]; // piece1
+echo "<br>";
+echo $pieces[1]; // piece2
+echo "<br>";
+// Example 2
+$data = "foo:*:1023:1000::/home/foo:/bin/sh";
+list($user, $pass, $uid, $gid, $gecos, $home, $shell) = explode(":", $data);
+
+echo $home; // foo
+echo "<br>";
+echo $pass; // *
